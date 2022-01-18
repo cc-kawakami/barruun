7a067f82d849c18ab60908be060f1ba9f169f1e1
cc-kawakami <kawakami-moeki@colorfulcompany.co.jp>
2022-01-18 16:22:08 +0900

    Fix config file loading error

--- lib/barruun/managers/utils.rb
+++ lib/barruun/managers/utils.rb
@@ -9,11 +9,10 @@ module Barruun
 
       def initialize(file_path, config_klass = Object.const_get("#{self.class.name.gsub("Managers", "Configurations")}"))
         @file_path = file_path
-        begin
-          @config = config_klass.new(YAML.load_file(file_path))
-        rescue
+        if !File.exists?(@file_path)
           raise self.class::ConfigFileLoadError, "Error loading config file: #{file_path}"
         end
+        @config = config_klass.new(YAML.load_file(file_path))
       end
 
       def call
